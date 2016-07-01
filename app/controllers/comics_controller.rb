1bd912b2d239c1512a0a24c6870eb77e0c2ac519
Matthew Scott <matthewruelscott@gmail.com>
2016-07-01 15:44:08 -0400

    Adds CRUD methods for publisher/comic

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -6,4 +6,37 @@ class ComicsController < ApplicationController
   def show
     @comic = Comic.find(params[:id])
   end
+
+  def new
+    @publisher = Publisher.find(params[:publisher_id])
+    @comic = Comic.new
+  end
+
+  def create
+    @publisher = Publisher.find(params[:publisher_id])
+    @comic = Comic.create!(comic_params.merge(publisher: @publisher))
+    redirect_to publisher_comic_path(@publisher, @comic)
+  end
+
+  def edit
+    @comic = Comic.find(params[:id])
+  end
+
+  def update
+    @comic = Comic.find(params[:id])
+    @publisher = Publisher.find(params[:publisher_id])
+    @comic.update(comic_params.merge(publisher: @publisher))
+    redirect_to publisher_comic_path(@comic.publisher, @comic)
+  end
+
+  def destroy
+    @comic = Comic.find(params[:id])
+    @comic.destroy
+    redirect_to comics_path
+  end
+
+  private
+  def comic_params
+    params.require(:comic).permit(:title, :issue_no, :authors, :artists, :year, :cover_url, :publisher)
+  end
 end
--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -10,4 +10,30 @@ class PublishersController < ApplicationController
   def new
     @publisher = Publisher.create
   end
+
+  def create
+    @publisher = Publisher.create!(publisher_params)
+    redirect_to (publisher_path(@publisher))
+  end
+
+  def edit
+    @publisher = Publisher.find(params[:id])
+  end
+
+  def update
+    @publisher = Publisher.find(params[:id])
+    @publisher.update(publisher_params)
+    redirect_to publisher_path(@publisher)
+  end
+
+  def destroy
+    @publisher = Publisher.find(parmas[:id])
+    @publisher.destroy
+    redirect_to publishers_path 
+  end
+
+  private
+  def publisher_params
+    params.require(:publisher).permit(:name, :nationality)
+  end
 end
