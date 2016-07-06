eecf93ab697e829f6a2e1e309e31ce86bcf68443
Matthew Scott <matthewruelscott@gmail.com>
2016-07-06 11:54:46 -0400

    style stuff

--- db/comic_data.rb
+++ db/comic_data.rb
@@ -27,7 +27,17 @@ def get_comic_data
       :year=>"2015",
       :cover_url=>"http://i.annihil.us/u/prod/marvel/i/mg/8/70/573a2aca62f10/detail.jpg",
       :publisher=>"Marvel Comics"
-      }],
+      },
+      {
+      :title=>"Infernal Man-Thing",
+      :issue_no=>"1",
+      :authors=>"Steve Gerber",
+      :artists=>"Kevin Nowlan",
+      :year=>"2012",
+      :cover_url=>"https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/27115/APR120649_1._SX640_QL80_TTD_.jpg",
+      :publisher=>"Marvel Comics"
+      }
+    ],
     }
       return comic_data
     end
