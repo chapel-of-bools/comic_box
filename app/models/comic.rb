caf905d94557e7bef2ef259393ebe3265174f654
Nick Olds <nolds9@yahoo.com>
2016-07-14 00:28:27 -0400

    adds feedback for ms project2

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,3 +1,7 @@
 class Comic < ActiveRecord::Base
   belongs_to :publisher
+
+  # NHO: can you think of any good model methods you might want to define?
+  # These would be a great way to define any ability to sort comics
+  # For example, you can make a method that sorts based on year range, etc..
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'publishers#index'
   resources :publishers do
+    # NHO: Nice job using nested resources!
     resources :comics
   end
-  resources :comics
+  # NHO: currently these routes work but there are no links to them
+  resources :comics # NHO: might want to limit these resources to only :index, :show
+
 end
